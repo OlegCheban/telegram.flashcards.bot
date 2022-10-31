c8f2f97d28319c54898575fd51feb1ea72f2463f
Чебан Олег <cheban.os7@gmail.com>
2022-10-31 19:26:20 +0300

    added last learn test

--- src/test/java/bot/botapi/handlers/learn/ProceedToRepetitionCallbackHandlerTest.java
+++ src/test/java/bot/botapi/handlers/learn/ProceedToRepetitionCallbackHandlerTest.java
@@ -10,10 +10,8 @@ import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageTe
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import ru.flashcards.telegram.bot.botapi.CallbackData;
-import ru.flashcards.telegram.bot.botapi.handlers.learn.ReturnToLearnCallbackHandler;
-import ru.flashcards.telegram.bot.db.dmlOps.ExerciseDataHandler;
+import ru.flashcards.telegram.bot.botapi.handlers.learn.ProceedToRepetitionCallbackHandler;
 import ru.flashcards.telegram.bot.db.dmlOps.FlashcardDataHandler;
-import ru.flashcards.telegram.bot.db.dmlOps.SpacedRepetitionNotificationDataHandler;
 import ru.flashcards.telegram.bot.db.dmlOps.dto.UserFlashcard;
 
 import java.lang.reflect.Field;
@@ -23,15 +21,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class ReturnToLearnCallbackHandlerTest {
-    @Mock
-    private FlashcardDataHandler flashcardDataHandler;
-    @Mock
-    private SpacedRepetitionNotificationDataHandler spacedRepetitionNotificationDataHandler;
-    @Mock
-    private ExerciseDataHandler exerciseDataHandler;
+public class ProceedToRepetitionCallbackHandlerTest {
     @Mock
     private Message message;
+    @Mock
+    private FlashcardDataHandler flashcardDataHandler;
     @Mock
     private CallbackQuery callbackQuery;
     @Mock
@@ -46,22 +40,13 @@ public class ReturnToLearnCallbackHandlerTest {
         when(callbackQuery.getMessage()).thenReturn(message);
         when(flashcardDataHandler.findUserFlashcardById(0L)).thenReturn(flashcard);
 
-        ReturnToLearnCallbackHandler handler = new ReturnToLearnCallbackHandler(callbackData);
-
+        ProceedToRepetitionCallbackHandler handler = new ProceedToRepetitionCallbackHandler(callbackData);
         Field flashcardDataHandlerField = handler.getClass().getDeclaredField("flashcardDataHandler");
         flashcardDataHandlerField.setAccessible(true);
         flashcardDataHandlerField.set(handler, flashcardDataHandler);
 
-        Field spacedRepetitionNotificationDataHandlerField = handler.getClass().getDeclaredField("spacedRepetitionNotificationDataHandler");
-        spacedRepetitionNotificationDataHandlerField.setAccessible(true);
-        spacedRepetitionNotificationDataHandlerField.set(handler, spacedRepetitionNotificationDataHandler);
-
-        Field exerciseDataHandlerField = handler.getClass().getDeclaredField("exerciseDataHandler");
-        exerciseDataHandlerField.setAccessible(true);
-        exerciseDataHandlerField.set(handler, exerciseDataHandler);
-
         List<BotApiMethod<?>> list = Mockito.spy(handler).handle(callbackQuery);
 
-        assertEquals("*Flashcard returned to learn*\n*word* \\[transcription]\n\ndescription\n\ntranslation", ((EditMessageText) list.get(0)).getText());
+        assertEquals("*Spaced repetition*\n*word* \\[transcription]\n\ndescription\n\ntranslation", ((EditMessageText) list.get(0)).getText());
     }
 }
